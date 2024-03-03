211d5384add36c1327759e68c7307048ad051be5
thirtiseven <ntlihy@gmail.com>
2024-03-03 22:39:29 +0800

    Add a screen moudle poc
    Signed-off-by: thirtiseven <ntlihy@gmail.com>

--- tests/test_screen_module.py
+++ tests/test_screen_module.py
@@ -1,18 +1,28 @@
 import queue
 import unittest
 
-from gazepointer.keypoint_module import ScreenModule
-
+from gazepointer.screen_module import ScreenModule
+from gazepointer.data_message import Data
 
 class TestKeypointDisplay(unittest.TestCase):
 
     def setUp(self):
         self.screen_queue = queue.Queue()
-        self.screen_module = ScreenModule(input_queue=self.keypoint_queue)
+        self.screen_module = ScreenModule(input_queue=self.screen_queue)
 
     def test_screen_module(self):
         try:
-            # TODO put Data in the screen queue and test that it works on the screen
+            # put Data in the screen queue and test that it works on the screen
+            for i in range(10):
+                self.screen_queue.put(Data(header="screen", payload=[100, 200]))
+                self.screen_queue.put(Data(header="screen", payload=[110, 220]))
+                self.screen_queue.put(Data(header="screen", payload=[130, 240]))
+                self.screen_queue.put(Data(header="screen", payload=[140, 260]))
+                self.screen_queue.put(Data(header="screen", payload=[150, 280]))
+                self.screen_queue.put(Data(header="screen", payload=[200, 300]))
+                self.screen_queue.put(Data(header="mouse", payload="click"))
+                self.screen_queue.put(Data(header="screen", payload=[300, 400]))
+
             self.screen_module.start()
         except KeyboardInterrupt:
             print("Caught KeyboardInterrupt, exiting...")
